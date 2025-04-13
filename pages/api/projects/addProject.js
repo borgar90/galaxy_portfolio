bd74a991c194b0f5b828417805241a7e75b51dd0
Borgar Flaen Stensrud <borgar90@gmail.com>
2025-04-13 11:46:17 +0200

    added alias

--- pages/api/projects/addProject.js
+++ pages/api/projects/addProject.js
@@ -1,4 +1,4 @@
-import { supabase } from "../lib/supabase"
+import { supabase } from "@/lib/supabase"
 
 export async function addProject({ title, description, image_url }) {
   const { data, error } = await supabase
